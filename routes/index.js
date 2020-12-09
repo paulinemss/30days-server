5517a7b6b2ffef1e533017541ed8f1f810b9b72b
paulinemss <masse.pauline@gmail.com>
2020-12-09 17:20:32 +0100

    Edit Form Route

--- routes/index.js
+++ routes/index.js
@@ -44,13 +44,23 @@ router.post('/courses/create', upload.single('image'), async (req, res) => {
     category
   } = req.body;
   const days = JSON.parse(req.body.days);
-  const { image } = req.file.path; 
+  let image; 
   const shortId = nanoid(10);
+
+  if (req.file) {
+    image = req.file.path; 
+  }
 
   try {
 
     console.log('req body', req.body);
     console.log('req file', req.file);
+
+    if (!title || !smallDescription || !longDescription || !author || !image || !category) {
+      res.status(400);
+      res.json({ err: 'Oops! Something went wrong' });
+      return;
+    }
 
     const newCourse = await Course.create({
       title,
@@ -73,6 +83,84 @@ router.post('/courses/create', upload.single('image'), async (req, res) => {
     res.sendStatus(400);
 
   }
+})
+
+router.put('/courses/edit/:id', upload.single('image'), async (req, res) => {
+  const { 
+    title, 
+    smallDescription, 
+    longDescription, 
+    author, 
+    category
+  } = req.body;
+  const days = JSON.parse(req.body.days);
+  let image; 
+  const shortId = nanoid(10);
+
+  if (req.file) {
+    image = req.file.path; 
+  }
+
+  try {
+
+    console.log('req body', req.body);
+    console.log('req file', req.file);
+
+    if (!title || !smallDescription || !longDescription || !author || !category) {
+      res.status(400);
+      res.json({ err: 'Oops! Something went wrong' });
+      return;
+    }
+
+    let updatedCourse; 
+
+    if (!req.file) {
+      updatedCourse = await Course.findOneAndUpdate(
+        {
+          shortId: req.params.id
+        }, 
+        {
+          title,
+          smallDescription,
+          longDescription,
+          category,
+          author,
+          days
+        }, 
+        { 
+          new: true
+        }
+      )
+    } else {
+      updatedCourse = await Course.findOneAndUpdate(
+        {
+          shortId: req.params.id
+        }, 
+        {
+          title,
+          image,
+          smallDescription,
+          longDescription,
+          category,
+          author,
+          days
+        }, 
+        { 
+          new: true
+        }
+      )
+    }
+
+    res.status(200);
+    res.json(updatedCourse);
+    
+  } catch (error) {
+
+    console.log('error', error);
+    res.sendStatus(400);
+
+  }
+
 })
 
 router.get('/courses/:id', async (req, res) => {
